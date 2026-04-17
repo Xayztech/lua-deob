a3b9c3692b39c715421d5807cb48986588e5ef61
Xayztech <xycoolcraft@gmail.com>
2026-04-17 15:01:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,8 @@ const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' })); 
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
     res.redirect('/deob');
 });
