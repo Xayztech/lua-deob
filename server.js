50cc2eb8f71c456f5bbb2b61757811c884dc0669
Xayztech <xycoolcraft@gmail.com>
2026-04-17 14:58:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.redirect('/deob');
+});
+
+app.get('/deob', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 // ==========================================
 // RESTful API ENDPOINT: /api/decode
 // Menerima JSON text ATAU File Upload
